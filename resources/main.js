136072d215c581d192236b66934a1f89d5161c7e
paveleveline <evelyn_lazau@yahoo.com>
2020-10-04 20:47:17 +0300

    Created menu-button

---
+++ resources/main.js
@@ -0,0 +1,14 @@
+const seeMenuBtn = document.getElementById('mobile-menu');
+const menuNavigation = document.getElementById('menu-mobile');
+seeMenuBtn.addEventListener('click', function() {
+    console.log('click');
+    menuNavigation.classList.remove('hidden');
+
+});
+
+const closeButton = document.getElementById('close-menu');
+closeButton.addEventListener('click', function() {
+    console.log ('click');
+    menuNavigation.classList.add('hidden');
+});
+
